de60bc9e7865c723399da1bebce290b1a1151226
Swapnil Gaikwad <swpnlgkwd@hotmail.com>
2021-07-03 15:14:03 +0530

    Server and Client Project Creation

---
+++ edurekagrpcsolun/Edurekha.GrpcClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Edurekha.GrpcClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
